6cffdab67ccf2b76b3ff73eb174babcbf9ac9384
mastronardif <mastronardif@gmail.com>
2019-08-06 22:09:33 -0400

    added .ignore, and updated timestamp.

--- MySwagger/Controllers/ValuesController.cs
+++ MySwagger/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace MySwagger.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "August 6th 2019 basement, value2" };
         }
 
         // GET api/values/5
